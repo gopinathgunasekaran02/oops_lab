dcc4ab79a70b74a4b36fbacbd8a20712931e04e3
gopi <gopinath.gunasekaran@revolvecorp.com>
2021-08-26 14:13:29 +0530

    Completed Ex-3

---
+++ Makefile
@@ -0,0 +1,5 @@
+lab1:
+	echo "lab_1_output"
+	cd lab_1
+	javac class_object_creation.java
+	java Main
---
+++ exp-3/abstract.java
@@ -0,0 +1,43 @@
+class OrderSummary{
+    OrderSummary(String name, int quantity, double total){
+        this.name=name;
+        this.quantity=quantity;
+        this.total=total;
+    }
+    String name;
+    int quantity;
+    double total;
+
+    @Override
+
+    public String toString(){
+        return "You've purshased "+ this.name+ " for $"+ this.total;
+    }
+}
+
+
+abstract class Payment{
+    abstract OrderSummary pay();
+}
+
+
+class Stripe extends Payment{
+    OrderSummary pay(){
+        return new OrderSummary("keyboard", 1, 250.0);
+    }
+}
+
+class Paypal extends Payment{
+    OrderSummary pay(){
+        return new OrderSummary("graphic card", 1, 55000.0);
+    }
+}
+
+class Main{
+    public static void main(String [] arg) {
+        Stripe payment_1= new Stripe();
+        Paypal payment_2= new Paypal();
+        System.out.println(payment_1.pay());
+        System.out.println(payment_2.pay());
+    }
+}
